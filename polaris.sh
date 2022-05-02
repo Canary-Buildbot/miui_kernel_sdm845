7fe0d31162a57c8243bb10a32d75b1e57d64cbe6
Canary233 <2096276316@qq.com>
2022-05-02 12:52:57 +0800

    Script:Fix dtb error log: make[3]: *** No rule to make target 'arch/arm64/boot/dts/qcom/polaris-mp-v2.1.dtb', needed by '__build'.  Stop. make[3]: *** Waiting for unfinished jobs.... make[2]: *** [../scripts/Makefile.build:656: arch/arm64/boot/dts/qcom] Error 2 make[1]: *** [arch/arm64/Makefile:186: dtbs] Error 2 make[1]: *** Waiting for unfinished jobs....

--- polaris.sh
+++ polaris.sh
@@ -4,7 +4,6 @@ git submodule update --init --recursive
 export PATH=/home/lu/clang/bin:${PATH}
 make O=out ARCH=arm64 polaris_defconfig -j$(nproc --all) LLVM=1
 make LLVM=1 -j$(nproc --all) O=out \ARCH=arm64 \CC=/home/lu/clang/bin/clang \
-	DTC_EXT=dtc \
 	CROSS_COMPILE=/home/lu/clang/bin/aarch64-linux-gnu- \
 	CROSS_COMPILE_ARM32=/home/lu/clang/bin/arm-linux-gnueabi- \
 	CROSS_COMPILE_COMPAT=/home/lu/clang/bin/arm-linux-gnueabi- \
